1ddabab1a9923356b70c87f98b256a13098d5d4a
Manaday Mavani <m.k.mavani@gmail.com>
2017-11-05 13:19:34 +0530

    Requirement of root element in JSX

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,9 +4,18 @@ console.log('App.js is running');
 
 // JSX - JavaScript XML
 var template = React.createElement(
-  'h1',
-  null,
-  'Indecision App'
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        'Indecision App'
+    ),
+    React.createElement(
+        'p',
+        null,
+        'This is some info'
+    )
 );
 var appRoot = document.getElementById('app');
 

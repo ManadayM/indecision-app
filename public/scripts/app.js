a346aca58619c12d7b443b9af126a44ad727a0df
Manaday Mavani <m.k.mavani@gmail.com>
2017-11-05 13:04:32 +0530

    Adds new folder src and public, adds babel

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,11 +1,12 @@
+'use strict';
+
 console.log('App.js is running');
 
 // JSX - JavaScript XML
-//var template = <p>This is JSX!</p>;
 var template = React.createElement(
-    "h1",
-    { id: 'app' },
-    "This is JSX"
+  'h1',
+  null,
+  'Indecision App'
 );
 var appRoot = document.getElementById('app');
 

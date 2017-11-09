75f96e68d0b91ba64f6fab3a8ef9f2469e126733
Manaday Mavani <mavani.manaday@investis.com>
2017-11-09 10:34:33 +0000

    Manual Data Binding

--- src/app.js
+++ src/app.js
@@ -20,25 +20,33 @@ let count = 0;
 
 const addOne = function(){
     count++;
+    renderCounterApp();
 };
 
 const minusOne = function(){
     count--;
+    renderCounterApp();
 };
 
 const reset = function(){
     count = 0;
+    renderCounterApp();
 };
-
-const template2 = (
-    <div>
-        <h1>Count: {count}</h1>
-        <button onClick={addOne}>+1</button>
-        <button onClick={minusOne}>-1</button>
-        <button onClick={reset}>Reset</button>
-    </div>
-);
 
 const appRoot = document.getElementById('app');
 
-ReactDOM.render(template2, appRoot);
+const renderCounterApp = () => {
+    
+    const template2 = (
+        <div>
+            <h1>Count: {count}</h1>
+            <button onClick={addOne}>+1</button>
+            <button onClick={minusOne}>-1</button>
+            <button onClick={reset}>Reset</button>
+        </div>
+    );
+    
+    ReactDOM.render(template2, appRoot);
+};
+
+renderCounterApp();

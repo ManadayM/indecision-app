62c87528aed521e58851c8c556479f25d02bfeab
Manaday Mavani <mavani.manaday@investis.com>
2017-11-10 13:41:26 +0000

    React components

--- src/app.js
+++ src/app.js
@@ -1,60 +1,72 @@
-console.log('App.js is running');
+// React enforces uppercase first letter for class name
+// the reason is when we keep it small React tries to create
+// html element out of it.
 
-// assignment
-const app = {
-    title: 'Indecision App',
-    subTitle: 'Put your life in the hands of a computer',
-    options: []
-};
+// var jsx = React.createElement(
+//     'div',
+//     null,
+//     React.createElement(
+//         'h1',
+//         null,
+//         'Title'
+//     ),
+//     React.createElement(Header, null),
+//     React.createElement(Header, null),
+//     React.createElement(Header, null)
+// );
 
-const onAddOption = (e) => {
-    e.preventDefault();
-
-    const $el = e.target.elements.option;
-    const option = $el.value;
-
-    console.log(e);
-
-    if(option){
-        app.options.push(option);
-        $el.value = '';
-        renderApp();
+class Header extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Indecision</h1>
+                <h2>Put your life in the hands of a computer</h2>
+            </div>
+        );
     }
-};
+}
 
-const onRemoveAll = () => {
-    app.options.length = 0;
-    app.options = [];
-    renderApp();
-};
+class Action extends React.Component {
+    render() {
+        return (
+            <div>
+                <button>What should I do?</button>
+            </div>
+        );
+    }
+}
 
-const onMakeDecision = () => {
-    const randomNum = Math.floor(Math.random() * app.options.length);
-    const option = app.options[randomNum];
-    alert(option);
-};
+class Options extends React.Component {
+    render(){
+        return (
+            <div>
+                <ol>
+                    <li>Item 1</li>
+                    <li>Item 2</li>
+                </ol>
+            </div>
+        );
+    }
+}
 
-const appRoot = document.getElementById('app');
-
-const renderApp = () => {
-
-    const template = (
-        <div>
-            <h1>{app.title}</h1>
-            {app.subTitle && <p>{app.subTitle}</p>}
-            <p>{app.options.length > 0 ? 'Here are your options': 'No options'}</p>
-            <button disabled={app.options.length == 0} onClick={onMakeDecision}>What should I do?</button>
-            <button disabled={app.options.length == 0} onClick={onRemoveAll}>Remove All</button>
-            <ol>
-                {app.options.map((option) => <li key={option}>{option}</li>)}
-            </ol>
-            <form onSubmit={onAddOption}>
+class AddOption extends React.Component {
+    render(){
+        return (
+            <div>
                 <input type="text" name="option" />
-                <button>Add Option</button>
-            </form>
-        </div>
-    );
-    ReactDOM.render(template, appRoot);
-};
+                <button>Add option</button>
+            </div>
+        );
+    }
+}
 
-renderApp();
+const jsx = (
+    <div>
+        <Header />
+        <Action />
+        <Options />
+        <AddOption />
+    </div>
+);
+
+ReactDOM.render(jsx, document.getElementById('app'));

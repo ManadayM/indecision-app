ab11a37791606d33bc279adeb43e45305ac5815e
Manaday Mavani <mavani.manaday@investis.com>
2017-11-10 08:47:24 +0000

    ES6 Classes Part 1

---
+++ src/playground/es6-classes-1.js
@@ -0,0 +1,18 @@
+
+class Person {
+
+    constructor(name = 'Anonymous', age=0) {
+        this.name = name;
+        this.age = age;
+    }
+
+    getDescription() {
+        return `${this.name} is ${this.age} years(s) old.`;
+    }
+}
+
+const me = new Person('Manaday Mavani', 30);
+console.log(me.getDescription());
+
+const other = new Person();
+console.log(other.getDescription());

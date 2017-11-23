8e2af9635683984cb1b18b9808be0dd46c78821e
Manaday Mavani <m.k.mavani@gmail.com>
2017-11-23 08:47:05 +0530

    Babel with webpack

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,13 @@ const config = {
     output: {
         filename: 'bundle.js',
         path: path.join(__dirname, 'public')
+    },
+    module: {
+        rules: [{
+            loader: 'babel-loader',
+            test: /\.js$/,
+            exclude: /node_modules/
+        }]
     }
 };
 

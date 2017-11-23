75f5800b56eec71f9e92123004c87ab4a25f7d2a
Manaday Mavani <m.k.mavani@gmail.com>
2017-11-23 06:22:32 +0530

    ES6 import/export

--- src/app.js
+++ src/app.js
@@ -1,1 +1,10 @@
-console.log('app.js is running!');
+// import { add, square } from './utils.js'
+
+// console.log('app.js is running!');
+// console.log(square(4));
+// console.log(add(1, 2));
+
+import { canDrink, isAdult } from './person.js';
+
+console.log(canDrink(19));
+console.log(isAdult(19));
---
+++ src/utils.js
@@ -0,0 +1,10 @@
+console.log('utils.js is running');
+
+// named export
+// export const square = (x) => x * x;
+
+const square = (x) => x * x;
+
+const add = (a, b) => a + b;
+
+export { square, add };
